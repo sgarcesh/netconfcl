a01d48e95314226bddec41385a7ce445fb5ff189
Cristian K. Cárdenas <ccardensa@outlook.com>
2018-11-30 10:54:10 -0300

    a

---
+++ Workshop Seguridad de Código/7-m07-redirects-exercise-files/before/10-Redirects/Redirect.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.UI;
+
+namespace _10_Redirects
+{
+  public partial class Redirect : Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      var url = Request.QueryString["url"];
+      
+      // ToDo: Log exit here.
+
+      Response.Redirect(url);
+    }
+  }
+}
